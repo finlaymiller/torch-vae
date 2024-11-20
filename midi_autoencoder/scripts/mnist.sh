c5ffe29c1e5e002f04c80e618aa589a377cf5f02
Finlay Miller <finlay@dal.ca>
2024-11-20 17:53:39 -0400

    got script launching train loop

--- midi_autoencoder/scripts/mnist.sh
+++ midi_autoencoder/scripts/mnist.sh
@@ -12,13 +12,6 @@ set -e
 # Store the time at which the script was launched, so we can measure how long has elapsed.
 start_time="$SECONDS"
 
-# Print disk usage report, to catch errors due to lack of file space.
-echo "------------------------------------"
-echo "df -h:"
-# Print header, then sort the rows alphabetically by mount point
-df -h --output=target,pcent,size,used,avail,source | head -n 1
-df -h --output=target,pcent,size,used,avail,source | tail -n +2 | sort -h
-echo ""
 echo "-------- Input handling ------------------------------------------------"
 date
 echo ""
@@ -56,25 +49,29 @@ source ~/.bashrc
 
 # Activate virtual environment
 ENVNAME="$PROJECT_NAME"
+CONDA_BASE=$(conda info --base)
+CONDA_SH="$CONDA_BASE/etc/profile.d/conda.sh"
+if [ -f "$CONDA_SH" ]; then
+    echo "Sourcing $CONDA_SH"
+    source "$CONDA_SH"
+else
+    echo "Error: $CONDA_SH not found. Make sure Conda is installed correctly."
+    exit 1
 echo "Activating conda environment $ENVNAME"
+fi
 conda activate "$ENVNAME"
 echo ""
 # Print env status (which packages you have installed - useful for diagnostics)
 # N.B. This script only prints things out, it doesn't assign any environment variables.
-echo "Running $PROJECT_NAME/scripts/report_env_config.sh"
-source "$PROJECT_NAME/scripts/report_env_config.sh"
+echo "Running $PROJECT_DIRN/scripts/report_env_config.sh"
+source "$PROJECT_DIRN/scripts/report_env_config.sh"
 
 # Set the JOB_LABEL environment variable
 echo "-------- Setting JOB_LABEL ---------------------------------------------"
 echo ""
 # Decide the name of the paths to use for saving this job
-if [ "$SLURM_ARRAY_TASK_COUNT" != "" ] && [ "$SLURM_ARRAY_TASK_COUNT" -gt 1 ];
-then
-    JOB_ID="${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}";
-else
-    JOB_ID="${SLURM_JOB_ID}";
-fi
-# Decide the name of the paths to use for saving this job
+SLURM_JOB_NAME=$PROJECT_NAME
+JOB_ID=$(date +"%Y%m%d%H%M%S")
 JOB_LABEL="${SLURM_JOB_NAME}__${JOB_ID}";
 echo "JOB_ID = $JOB_ID"
 echo "JOB_LABEL = $JOB_LABEL"
@@ -86,7 +83,7 @@ echo ""
 # Vector provides a fast parallel filesystem local to the GPU nodes, dedicated
 # for checkpointing. It is mounted under /checkpoint. It is strongly
 # recommended that you keep your intermediary checkpoints under this directory
-CKPT_DIR="/checkpoint/${SLURM_JOB_USER}/${SLURM_JOB_ID}"
+CKPT_DIR="checkpoints/$JOB_ID"
 echo "CKPT_DIR = $CKPT_DIR"
 CKPT_PTH="$CKPT_DIR/checkpoint_latest.pt"
 echo "CKPT_PTH = $CKPT_PTH"
@@ -109,7 +106,7 @@ elif [[ -d "/scratch/ssd004/scratch/$SLURM_JOB_USER" ]];
 then
     OUTPUT_DIR="/scratch/ssd004/scratch/$SLURM_JOB_USER"
 else
-    OUTPUT_DIR=""
+    OUTPUT_DIR="."
 fi
 if [[ "$OUTPUT_DIR" != "" ]];
 then
@@ -129,73 +126,20 @@ fi
 # Save a list of installed packages and their versions to a file in the output directory
 conda env export > "$CKPT_DIR/environment.yml"
 pip freeze > "$CKPT_DIR/frozen-requirements.txt"
-
-if [[ "$SLURM_RESTART_COUNT" > 0 && ! -f "$CKPT_PTH" ]];
-then
-    echo ""
-    echo "====================================================================="
-    echo "SLURM SCRIPT ERROR:"
-    echo "    Resuming after pre-emption (SLURM_RESTART_COUNT=$SLURM_RESTART_COUNT)"
-    echo "    but there is no checkpoint file at $CKPT_PTH"
-    echo "====================================================================="
-    exit 1;
-fi
-
 echo ""
-echo "------------------------------------"
+echo "------------------------------------------------------------------------"
 elapsed=$(( SECONDS - start_time ))
 eval "echo Running total elapsed time for restart $SLURM_RESTART_COUNT: $(date -ud "@$elapsed" +'$((%s/3600/24)) days %H hr %M min %S sec')"
 echo ""
 echo "-------- Begin main script ---------------------------------------------"
 date
 echo ""
-# Store the master node's IP address in the MASTER_ADDR environment variable,
-# which torch.distributed will use to initialize DDP.
-export MASTER_ADDR=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
-echo "Rank 0 node is at $MASTER_ADDR"
-
-# Use a port number automatically selected from the job id number.
-# This will let us use the same port for every task in the job without having
-# to create a file to store the port number.
-# We don't want to use a fixed port number because that would lead to
-# collisions between jobs when they are scheduled on the same node.
-# We only use ports in the range 49152-65535 (inclusive), which are the
-# Dynamic Ports, also known as Private Ports.
-MASTER_PORT="$(( $SLURM_JOB_ID % 16384 + 49152 ))"
-if ss -tulpn | grep -q ":$MASTER_PORT ";
-then
-    # The port we selected is in use, so we'll get a random available port instead.
-    echo "Finding a free port to use for $SLURM_NNODES node training"
-    MASTER_PORT="$(python -c 'import socket; s=socket.socket(); s.bind(("", 0)); print(s.getsockname()[1])')";
-fi
-export MASTER_PORT;
-echo "Will use port $MASTER_PORT for c10d communication"
-
-export WORLD_SIZE="$(($SLURM_NNODES * $SLURM_GPUS_ON_NODE))"
-echo "WORLD_SIZE = $WORLD_SIZE"
-
-# NCCL options ----------------------------------------------------------------
-
-# This is needed to print debug info from NCCL, can be removed if all goes well
-export NCCL_DEBUG=INFO
-
-# This is needed to avoid NCCL to use ifiniband, which the cluster does not have
-export NCCL_IB_DISABLE=1
-
-# This is to tell NCCL to use bond interface for network communication
-if [[ "${SLURM_JOB_PARTITION}" == "t4v2" ]] || [[ "${SLURM_JOB_PARTITION}" == "rtx6000" ]];
-then
-    echo "Using NCCL_SOCKET_IFNAME=bond0 on ${SLURM_JOB_PARTITION}"
-    export NCCL_SOCKET_IFNAME=bond0
-fi
-
-# Set this when using the NCCL backend for inter-GPU communication.
-export TORCH_NCCL_BLOCKING_WAIT=1
-# -----------------------------------------------------------------------------
 
 # Multi-GPU configuration
+SLURM_JOB_NUM_NODES=1
+SLURM_GPUS_ON_NODE=1
+SLURM_CPUS_PER_GPU=4
 echo ""
-echo "Main script begins via torchrun with host tcp://${MASTER_ADDR}:$MASTER_PORT with backend NCCL"
 if [[ "$SLURM_JOB_NUM_NODES" == "1" ]];
 then
     echo "Single ($SLURM_JOB_NUM_NODES) node training ($SLURM_GPUS_ON_NODE GPUs)"
@@ -220,14 +164,10 @@ echo ""
 # process ID and wait for it to finish before continuing) so the main process
 # can handle the SIGUSR1 signal. Otherwise if a child process is running, the
 # signal will be ignored.
-srun -N "$SLURM_NNODES" --ntasks-per-node=1 \
-    torchrun \
+torchrun \
+    "$SCRIPT_PATH" \
     --nnodes="$SLURM_JOB_NUM_NODES" \
     --nproc_per_node="$SLURM_GPUS_ON_NODE" \
-    --rdzv_id="$SLURM_JOB_ID" \
-    --rdzv_backend=c10d \
-    --rdzv_endpoint="$MASTER_ADDR:$MASTER_PORT" \
-    "$SCRIPT_PATH" \
     --cpu-workers="$SLURM_CPUS_PER_GPU" \
     --seed="$SEED" \
     --checkpoint="$CKPT_PTH" \
@@ -239,7 +179,7 @@ child="$!"
 wait "$child"
 
 echo ""
-echo "------------------------------------"
+echo "----------------------------------------------------------------------------"
 elapsed=$(( SECONDS - start_time ))
 eval "echo Running total elapsed time for restart $SLURM_RESTART_COUNT: $(date -ud "@$elapsed" +'$((%s/3600/24)) days %H hr %M min %S sec')"
 echo ""
